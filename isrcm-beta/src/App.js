ef4cc6892a91393406e3cc7b4c103d0d2d0a6229
Chriiistian <christian.hernandez.aranda@alumnos.uta.cl>
2022-10-15 23:22:49 -0300

    update 0.0.7
    se añadio la base de la pagina principal

--- isrcm-beta/src/App.js
+++ isrcm-beta/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
+import Ver_Emp from './components/Ver_emp';
 
 function App() {
   return (
     <div id='background'>
-      <Login/>
+      {"<Login/>"}
+      <Ver_Emp/>
+      
     </div>
   );
 }
